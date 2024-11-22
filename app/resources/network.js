42a0599e1cc044c20dd189652247ace40223584d
FarelDev5 <farel.developer55@gmail.com>
2024-11-22 14:09:23 +0800

    Add files via upload

--- app/resources/network.js
+++ app/resources/network.js
@@ -1,1 +1,17 @@
+function restart(){
+  location.reload(true)
+}
+function checkInternetConnection() {
+  if (navigator.onLine) {
+    document.querySelector('.network-err').style.display = 'none'
+  } else {
+    document.querySelector('.network-err').style.display = 'grid'
+  }
+}
 
+// Jalankan fungsi saat halaman dimuat
+checkInternetConnection();
+
+// Tambahkan event listener untuk memantau perubahan status koneksi
+window.addEventListener('online', () => console.log("Koneksi internet tersambung kembali."));
+window.addEventListener('offline', () => console.log("Koneksi internet terputus."));
